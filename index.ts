8d39131acfbd060ae8d93ed21145639b6ef57f6a
Humafhd <fhuma302@gmail.com>
2024-03-11 17:56:35 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+let hatprice =19;
+// console.log(` price of hat is ${hatprice}`);//temp.liter. n call.of var
+// console.log(199);// prints bcx console prints
+// let bootprice=56;
+// console.log(`${bootprice}`);
+//conditional statements
+
+// if (hatprice==19){
+//     console.log('you are right');
+    
+// }else {console.log('you are wrong');
+// }
+
+//changes at the vari.values print and select what to print
+
+//if-else and else-if
+if (hatprice==19){                    // same value as abv by let declaration
+    console.log('you are right');
+ } else if (hatprice =30){
+        console.log("the price is 30/=Rs");
+    }
+    else {console.log('you are wrong');
+} //changes at variable declaration. will show what to print
+//if its 19..u r ri8,//if its 3o the price is 30
+//if given any other value other then 19 it will say price is 30
